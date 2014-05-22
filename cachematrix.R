e53f8a22b06ec6aeee0caad87cc545d00f03a4f7
Oliver Wise <oliver_wise@yahoo.com>
2014-05-22 12:51:24 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## This code contains two functions: "makeCacheMatrix" and "cacheSolve." t
+## This code contains two functions: "makeCacheMatrix" and "cacheSolve." The "makeCacheMatrix" function takes advantage of the
+##<<- operator so that assignments made within the environment of that function apply outside of it, so that such values can
+##be used in the "cacheSolve" function.
 
-## Write a short comment describing this function
-
+## The makeCacheMatrix" function creates a list of the outputs of four functions. It also creates the cache variable m.
+## set - sets the value of a matrix.
+## get - prints the value of the matrix assigned to "set" 
+## setinverse - sets the value of the inverse of the matrix
+## getinverse - 
 makeCacheMatrix <- function(x = matrix()) {
     m<-NULL
     set<-function(y){
